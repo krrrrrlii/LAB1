6798363f647febb40a7b1a62b45f72f1ac26494d
Елизавета <74406940+krrrrrlii@users.noreply.github.com>
2023-09-21 23:43:01 +0500

    Add files via upload

--- hello.js
+++ hello.js
@@ -3,6 +3,6 @@ const calculateVolume = () => {
 
     let area = (5*(length**3)*(3+Math.sqrt(5)))/(12);
 
-    document.getElementById("result").innerHTML = area;
+    document.getElementById("result").innerHTML = volume;
     document.getElementById("button").style = "colour: #azure";
 }
