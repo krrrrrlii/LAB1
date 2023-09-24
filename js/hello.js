ac8a08ab99d8e89e232359b39e8ce0c7178f8405
Елизавета <74406940+krrrrrlii@users.noreply.github.com>
2023-09-24 13:33:47 +0300

    Delete js directory

--- js/hello.js
+++
@@ -1,8 +0,0 @@
-const calculateVolume = () => {
-    let length = document.getElementById("length").value;
-
-    let volume = (5*(length**3)*(3+Math.sqrt(5)))/(12);
-
-    document.getElementById("result").innerHTML = volume;
-    document.getElementById("button").style = "colour: #azure";
-}
